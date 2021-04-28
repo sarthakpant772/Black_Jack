49fb9e86c39d1b0408315416ced9ff51f3cc21b2
sarthakpant772 <sarthak.pant31@gmail.com>
2021-04-28 22:53:48 +0530

    added structure

---
+++ Black_Jack.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+class Game{
+
+
+    public:
+
+    void WelcomeScreen(){
+
+    }
+
+    void MainGame(){
+
+    }
+
+    void Rules(){
+
+    }
+    
+    void Login(){
+
+    }
+
+    void signup(){
+
+    }
+
+};
+int main(){
+
+}
